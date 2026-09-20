b2254410cb99e4fa2ba8abfe556e2be27bf14738
SirQuestalot <sircrazynut@gmail.com>
2026-09-20 01:59:05 -0600

    Update packformat_lookup.py

--- .github/scripts/packformat_lookup.py
+++ .github/scripts/packformat_lookup.py
@@ -1,17 +1,21 @@
-def packformat_lookup(input_version, lookup_path):            #lookup_path should be somewhere like .github/lookup.txt
+def packformat_lookup(input_version, lookup_path):            #included lookup_path should be somewhere like .github/lookup.txt
     with open(lookup_path, 'r') as f:                         #with repo license at top i.e. license: GPL-3.0-or-later
-        lines = f.read().split('\n')                          #and lines of mc version with correspoding pack format number
-                                                              #i.e. 26.2, 88                 
-    for line in lines:
+        lines = f.read().split('\n')                          #and lines of mc version with correspoding pack format number i.e. 26.2, 88
+    
+    for line_number, line in enumerate(lines, start=1):       #keep track of line number for each line
         trimmed = line.strip()                                #clean up hidden text formatting
         if not trimmed or trimmed.startswith('license:'):     #skip empty lines and license declaration
             continue
 
-        lookup_version, packformat_str = [s.strip() for s in trimmed.split(',')]  #split each line into mc version and packformat number
-        if lookup_version == input_version:                                       #check requested version against lookup table
-            return {'packformat': int(packformat_str)}                            #packformat needs to be converted from string to integer
+        parts = [s.strip() for s in trimmed.split(',')]                                     #split each line into mc version and packformat number
+        if len(parts) != 2:                                                                 #each line should only have two values, mc version and pack format number i.e. 26.2, 88
+            raise ValueError(f'Invalid line "{line_number} {trimmed}" in lookup file')      #throw error with line# and line content for typos/incorrect data entry in lookup_path
+        lookup_version, packformat_str = parts
 
-    if version == '1.0-1.5.x':
-        return {'packformat': None}  #no pack format number for pre-1.6
+        if lookup_version == input_version:                 #validate requested version against lookup table
+            return {'packformat': int(packformat_str)}      #packformat needs to be converted from string to integer
 
-    raise ValueError(f'No valid lookup entry for "{input_version}"')  #error for typos/invalid version sequence in request
+    if input_version == '1.0-1.5.x':
+        return {'packformat': None}     #no pack format number for pre-1.6
+
+    raise ValueError(f'No matching lookup entry for "{input_version}"')     #throw error for typos/invalid version sequence in input_version request
